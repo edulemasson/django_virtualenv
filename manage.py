95741ce56708e75adaf81f7362e1e49a80ced597
Eduardo Le Masson <eduardolemasson@midiahome.com.br>
2016-04-03 23:42:14 -0300

    New Django Boots Screen when execute manage.py

--- manage.py
+++ manage.py
@@ -7,4 +7,11 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    os.system('clear')
+
+    print " _                 __  _     _   _   _ ___ __"
+    print "| \   |  /\  |\ | /__ / \   |_) / \ / \ | (_"
+    print "|_/ \_| /--\ | \| \_| \_/   |_) \_/ \_/ | __)"
+    print "                         by Eduardo Le Masson"
+
     execute_from_command_line(sys.argv)
